70320e0f8134c1acbba10c6bad711acc15045647
semonalbert <semonalbert@yeah.net>
2016-04-29 21:14:20 +0800

    drop db file, add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+version = '0.1.0'
+name='wswebserver'
+
+setup(
+    name=name,
+    version=version,
+    description='WsWebServer.'
+    long_description='A websocket proxy and web app server.',
+    packages=['wswebserver']
+)
